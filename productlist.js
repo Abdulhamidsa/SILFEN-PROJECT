2e12991507abd9f9245b07282d9e2c02cd4504c9
Jonas Holbech <abdu3793@stud.kea.dk>
2022-04-21 22:29:02 +0200

    add home link

--- productlist.js
+++ productlist.js
@@ -5,40 +5,40 @@ const urlParams = new URLSearchParams(window.location.search);
 const url = "DATABASE URL GOES HERE";
 
 const products = {
-  headers: {
-    "x-apikey": "API KEY GOES HERE",
-  },
+    headers: {
+        "x-apikey": "API KEY GOES HERE",
+    },
 };
 
 fetch(url, products)
-  .then((res) => res.json())
-  .then((data) => handleProductList(data));
+    .then((res) => res.json())
+    .then((data) => handleProductList(data));
 
 function handleProductList(data) {
-  console.log(data);
-  data.forEach(showProduct);
+    console.log(data);
+    data.forEach(showProduct);
 }
 
 function showProduct(product) {
-  // grab template
-  const template = document.querySelector("template").content;
+    // grab template
+    const template = document.querySelector("template").content;
 
-  // clone template
-  const myClone = template.cloneNode(true);
+    // clone template
+    const myClone = template.cloneNode(true);
 
-  // change content
-  myClone.querySelector(".productName").textContent = product.productname;
-  myClone.querySelector(
-    "img"
-  ).src = `http://carolinethostrup.dk/CustomiseMeImages/${product.imageurl}`;
-  myClone.querySelector(".price").textContent = `DKK ${product.price},-`;
+    // change content
+    myClone.querySelector(".productName").textContent = product.productname;
+    myClone.querySelector(
+        "img"
+    ).src = `http://carolinethostrup.dk/CustomiseMeImages/${product.imageurl}`;
+    myClone.querySelector(".price").textContent = `DKK ${product.price},-`;
 
-  myClone.querySelector("img").alt = `${product.name}`;
-  myClone
-    .querySelector(".productButton")
-    .setAttribute("href", `productPage.html?_id=${product._id}`);
+    myClone.querySelector("img").alt = `${product.name}`;
+    myClone
+        .querySelector(".productButton")
+        .setAttribute("href", `productPage.html?_id=${product._id}`);
 
-  // select parent & append
-  const parent = document.querySelector("main");
-  parent.appendChild(myClone);
+    // select parent & append
+    const parent = document.querySelector("main");
+    parent.appendChild(myClone);
 }
