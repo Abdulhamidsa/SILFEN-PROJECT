7002e6bc1eac264c1afc83af1d5d8ab0718e5284
Jonas Holbech <abdu3793@stud.kea.dk>
2022-04-24 09:25:42 +0200

    finished-landingPage

---
+++ main.js
@@ -0,0 +1,29 @@
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    loadData();
+}
+async function loadData() {
+    const response = await fetch(
+        "https://carolinethostrup.dk/Theme09-Content/Silfen-WP/wp-json/wp/v2/bag?per_page=2&categories=7"
+    );
+    console.log("response2", response);
+    const bagData = await response.json();
+    displaybag(bagData);
+}
+async function displaybag(userJSON) {
+    userJSON.forEach((bags) => {
+        const template = document.querySelector("#BagProductTemplate").content;
+        const copy = template.cloneNode(true);
+        copy.querySelector(".product-name").textContent = bags.title.rendered;
+        copy.querySelector(".product-price").textContent = " DKK " + bags.price;
+        copy.querySelector(".color1").style.backgroundColor = bags.color1;
+        copy.querySelector(".color2").style.backgroundColor = bags.color2;
+        copy.querySelector(".color3").style.backgroundColor = bags.color3;
+        copy.querySelector(".color-name1").textContent = bags.colorname1;
+        copy.querySelector(".product-img1").src = bags.image1.guid;
+        copy.querySelector(".product-img2").src = bags.image2.guid;
+        const parent = document.querySelector("main");
+        parent.appendChild(copy);
+    });
+}
